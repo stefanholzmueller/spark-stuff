5bd001e4a4173213722cae1c8da1b6d9c7ef5961
Stefan Holzmüller <stefanholzmueller@github>
2015-04-05 13:29:46 +0200

    getting started with Collaborative Filtering

---
+++ src/main/scala/spark/Recommendation.scala
@@ -0,0 +1,49 @@
+package spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.mllib.recommendation.Rating
+
+// run with bin\spark-submit --class spark.Recommendation path/to/target/jar
+object Recommendation {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("ALS example")
+    val sc = new SparkContext(conf)
+
+    val ratings = sc.parallelize(List(      Rating(1, 1, 5),      Rating(1, 2, 1),      Rating(2, 1, 1),      Rating(2, 2, 5),      Rating(3, 1, 5)));
+    // Load and parse the data
+    //    val data = sc.textFile("data/mllib/als/test.data")
+    //    val ratings = data.map(_.split(',') match {
+    //      case Array(user, item, rate) =>
+    //        Rating(user.toInt, item.toInt, rate.toDouble)
+    //    })
+
+    // Build the recommendation model using ALS
+    val rank = 10
+    val numIterations = 20
+    val lambda = 0.01
+    val model = ALS.train(ratings, 1, 20, 0.01)
+
+    // Evaluate the model on rating data
+    val usersProducts = ratings.map {      case Rating(user, product, rate) =>        (user, product)    }
+    val predictions =      model.predict(usersProducts).map {        case Rating(user, product, rate) =>          ((user, product), rate)      }
+    val ratesAndPreds = ratings.map {      case Rating(user, product, rate) =>        ((user, product), rate)    }.join(predictions)
+    // Array(((1,1),(5.0,4.994369365812157))
+    //  ((1,2),(0.0,0.0018383536502297737)), 
+    // (2,1),(1.0,1.0003445490826683)), ((3,1),(4.0,3.9978672872717453)),
+    //, ((2,2),(4.0,3.9914009329176627)))
+    // ((3,1),(4.0,3.9978672872717453)),
+    // predict(3,2) => 0.6563867404903589
+    
+    val MSE = ratesAndPreds.map {      case ((user, product), (r1, r2)) =>        val err = (r1 - r2);        err * err    }.mean()
+    println("Mean Squared Error = " + MSE)
+
+    // Save and load model
+    //    model.save(sc, "myModelPath")
+    //    val sameModel = MatrixFactorizationModel.load(sc, "myModelPath")
+  }
+}
---
+++ src/test/scala/spark/CollaborativeFilteringTest.scala
@@ -0,0 +1,42 @@
+package spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.mllib.recommendation.Rating
+import org.junit.Test
+import org.junit.Assert
+import org.junit.Before
+
+class ALSTest {
+
+  def testWithCleanSlate(body: SparkContext => Unit) = {
+    System.clearProperty("spark.driver.port")
+    System.clearProperty("spark.hostPort")
+    val sc = new SparkContext("local", getClass().getSimpleName())
+    try {
+      body(sc)
+    } finally {
+      sc.stop()
+      System.clearProperty("spark.driver.port")
+      System.clearProperty("spark.hostPort")
+    }
+  }
+
+  @Test
+  def correctPrediction = {
+    testWithCleanSlate { sc =>
+      val ratings = sc.parallelize(List(Rating(1, 1, 5), Rating(1, 2, 1), Rating(2, 1, 1), Rating(2, 2, 5), Rating(3, 1, 5)));
+      val rank = 1
+      val iterations = 20 // 10 => predict 6.065
+      val lambda = 0.01
+      val blocks = 1
+      val seed = 123456789
+      val model = ALS.train(ratings, 1, 20, 0.01, 1, 123456789)
+      Assert.assertEquals(4.953176333258341, model.predict(3, 2), 0.00000001)
+    }
+  }
+
+}
